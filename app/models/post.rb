5d8fd738c82e2f9e990f66e5ab25f02ee424e71d
James Quon <joquon@gmail.com>
2012-02-07 01:16:57 -0700

    added validation for both fields, home view replaced

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title,	:presence => true,
+			:length => { :minimum => 5 }
+	    :content,	:presence => true,
+			:length => { :minimum => 5 :maximum => 400 }
 end
